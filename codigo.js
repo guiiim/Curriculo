5fbc3e94be9c4a1be5bb4f74157e5f5231e3eb53
Guilherme Augusto Magalhães <80900767+guiiim@users.noreply.github.com>
2023-02-22 15:42:51 -0300

    Add files via upload

---
+++ codigo.js
@@ -0,0 +1,28 @@
+function ocultar()
+{
+    document.getElementById('correto').style.display = 'none';
+}
+
+function digitarResp()
+{
+    if(document.getElementById('resposta').value != "")
+    {
+        document.getElementById('btSubmeter').style.display = 'block';   
+    }
+}
+
+function submeterResp()
+{
+    if(document.getElementById('resposta').value == 5)
+    {
+        document.getElementById('login').style.display = 'none';
+        document.getElementById('correto').style.display = 'block';
+
+        document.getElementById("login").style.backgroundColor = "lightblue";
+        document.body.style.background = "lightblue";
+    }
+    else
+    {
+        alert("Não foi dessa vez!");
+    }
+}
